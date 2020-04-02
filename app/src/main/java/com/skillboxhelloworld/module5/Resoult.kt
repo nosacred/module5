6c8597fde0870c7efdd4f3493184ed8ec3a2372b
NoSacred <und3rrrr@gmail.com>
2020-04-03 00:27:04 +0300

    hw 5.1.1 and 5.1.2

--- app/src/main/java/com/skillboxhelloworld/module5/Resoult.kt
+++ app/src/main/java/com/skillboxhelloworld/module5/Resoult.kt
@@ -1,19 +1,18 @@
 package com.skillboxhelloworld.module5
 
-sealed class Resoult<out T,in R>
+sealed class Resoult<out T, R>
 
-    data class Success<T:Number, R:String>(var item: T) : Resoult<T, R>()
+    data class Success<T, R>(var defaultValue: T) : Resoult<T, R>()
 
-    data class Error<T:Any, R:String>(var item: R) : Resoult<T, R>()
+    data class Error<T, R>(var item: R) : Resoult<T, R>()
 
-    fun getResoult(resoult: Resoult<Any,String>) : Resoult<Int, String>{
-         when (resoult){
-            is Success ->
-            is Error ->
-        }
-        return
+    fun getResoult() : Resoult<Int, String>{
+      val item : Resoult<Int, String> = Success(12)
+        return item
     }
 
 fun main() {
+    val red :Resoult<Int,String> = Success(12)
+    println(red.toString())
 
 }
